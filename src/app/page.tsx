b95d52b8cd6b16a13e0421b285e693a6d96e89ba
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-01-08 03:01:33 +0600

    Just Shadcn is implemented

---
+++ src/app/page.tsx
@@ -0,0 +1,21 @@
+import { ModeToggle } from "@/components/ModeToggle"
+import { Button } from "@/components/ui/button"
+export default function Home() {
+  return (
+  <>
+    <h1> This is Home Page </h1>
+    <Button>Click me</Button>
+<ModeToggle/>
+ <h2>
+  this is headi2 
+ </h2>
+
+ <h2>
+  this is headi2 
+ </h2>
+
+ <div className="bg-primary text-primary-foreground">Hello</div>
+
+  </>
+  )
+}
