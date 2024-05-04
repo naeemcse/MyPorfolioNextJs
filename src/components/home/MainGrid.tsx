55f8e213c0e6e8a0436d16758d5f497c690de42f
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-05-05 02:22:17 +0600

    home page is designed well but inter activitey is remain

--- src/components/home/MainGrid.tsx
+++ src/components/home/MainGrid.tsx
@@ -26,36 +26,124 @@ const MainGrid = () => {
               </h1>
             </a>
           </div>
-        </div>
-        <div className="col-span-12 md:col-span-7">
             <div className="container">
-               <div className="flex justify-center gap-5">
-                <div className="overflow-hidden rounded-lg">
-                    <img className="photo w-auto h-[100px]" src="/asset/images/web_app_cover.png" alt="" />
-
+                <div className="flex gap-5 justify-center items-center ">
+                    <div className="p-2 bg-muted rounded-full photo ">
+                        <Image className="rounded-full cursor-pointer" src="/asset/images/react_icon.png" alt="React "
+                               title="React" width={50} height={50}/>
+                    </div>
+                    <div className="p-2 bg-muted rounded-full photo">
+                        <Image className="cursor-pointer" src="/asset/images/nextjs_icon.png" alt="React " title="React"
+                               width={50} height={50}/>
+                    </div>
+                    <div className="p-2 bg-muted rounded-full photo cursor-pointer">
+                        <Image className="rounded-full " src="/asset/images/javascript_icon.png" alt="React "
+                               title="React" width={50} height={50}/>
+                    </div>
                 </div>
-                <div className="overflow-hidden rounded-lg">
-                    <img className="photo w-auto h-[100px]" src="/asset/images/web_app_cover2.png" alt="" />
-
-                </div>
-
-               </div>
-               <div className="mt-[30px]">
-                <p className="text-sm text-muted"> Show Case View </p>
+                <p className="text-secondary-foreground">Specialization </p>
                 <a
-              href="/project"
-              className="no-underline"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <h1 className="uppercase text-secondary-foreground">
-                    Projcect
-              </h1>
-            </a>
-               </div>
+                    href="/service"
+                    className="no-underline"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    <h1 className="uppercase text-secondary-foreground">
+                        Services
+                    </h1>
+                </a>
+            </div>
+            <div className="container">
+                <div className="grid-12">
+                    <div className="col-span-6">
+                        <h1 className='text-primary'> 4+ </h1>
+                        <p>Years of Experience</p>
+                    </div>
+                    <div className="col-span-6">
+                        <h1 className='text-primary'> 20+ </h1>
+                        <p>Project Completed </p>
+
+                    </div>
+                </div>
 
             </div>
+
         </div>
+          <div className="col-span-12 md:col-span-7">
+              <div className="container">
+                  <div className="flex justify-center gap-5">
+                      <div className="overflow-hidden rounded-lg">
+                          <img className="photo w-auto h-[100px]" src="/asset/images/web_app_cover.png" alt=""/>
+                      </div>
+                      <div className="overflow-hidden rounded-lg">
+                          <img className="photo w-auto h-[100px]" src="/asset/images/web_app_cover2.png" alt=""/>
+                      </div>
+
+                  </div>
+                  <div className="mt-[30px]">
+                      <p className=""> Show Case View </p>
+                      <a
+                          href="/project"
+                          className="no-underline"
+                          target="_blank"
+                          rel="noopener noreferrer"
+                      >
+                          <h1 className="uppercase text-secondary-foreground">
+                              Projcect
+                          </h1>
+                      </a>
+                  </div>
+
+              </div>
+                  <div className="grid-12">
+                      <div className="col-span-6 bg-primary container">
+                          <p className="text-sm "> Visit My Blog  </p>
+                          <a
+                              href="/blog"
+                              className="no-underline"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                          >
+                              <h1 className="uppercase text-secondary-foreground">
+                                  Blog
+                              </h1>
+                          </a>
+                      </div>
+                      <div className="col-span-6 container">
+
+                          <p className="text-sm "> Let's Work  </p>
+                          <a
+                              href="/contact"
+                              className="no-underline"
+                              target="_blank"
+                              rel="noopener noreferrer"
+                          >
+                              <h1 className="uppercase text-secondary-foreground text-2xl">
+                                  Contact
+                              </h1>
+                          </a>
+                      </div>
+                  </div>
+              <div className="container bg-[url('/asset/images/rivew.jpg')]  bg-no-repeat  h-1/3 bg-center bg-opacity-600 mb-40" >
+<div className="flex gap-5">
+    <div>
+        <p className=""> What Client Says?</p>
+        <a
+            href="/testimonial"
+            className="no-underline"
+            target="_blank"
+            rel="noopener noreferrer"
+        >
+            <h1 className="uppercase text-secondary-foreground">
+             Testimonial
+            </h1>
+        </a>
+
+    </div>
+</div>
+              </div>
+
+          </div>
       </div>
     </div>
   );
