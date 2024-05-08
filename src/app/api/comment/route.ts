d7f767483026d60f9fb63ba669706ed318d6d85b
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-05-09 01:49:29 +0600

    message and comment is added in api

--- src/app/api/comment/route.ts
+++ src/app/api/comment/route.ts
@@ -1,30 +1,36 @@
+// @ts-nocheck
 import { connectToDatabase } from "@/helper/server-helper";
 import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-export const GET = async(req:Request,res:NextResponse)=>{
+
+export const POST = async(req:Request,res)=>{
     try{
+        let {searchParams} = new URL(req.url) ;
+        let postId = searchParams.get('postId') ;
+        const reqBody = await req.json();
+        reqBody.postId = postId;
         await connectToDatabase();
-        const result = await prisma.post.findMany({
-            orderBy: {
-                createdAt: 'desc'
-            },include:{comments:true}
+        const result = await prisma.comment.create({
+          data: reqBody 
         });
         return NextResponse.json({
             status: "success",
-            message: "Blog post fetched successfully",
+            message: "Comment is done",
             data: result
         });
 
     }catch(error){
         console.log(error);
         return NextResponse.json({
-            message: "Something went wrong with the blog post"
+            message: "Failed to comment",
+            error: error
         });
     }
     finally{
         await prisma.$disconnect();
 
     }
+
 }
--- src/app/api/message/route.ts
+++ src/app/api/message/route.ts
@@ -1,30 +1,33 @@
+// @ts-nocheck
 import { connectToDatabase } from "@/helper/server-helper";
 import { NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client'
 
 const prisma = new PrismaClient()
-export const GET = async(req:Request,res:NextResponse)=>{
+
+export const POST = async(req:Request,res)=>{
     try{
+        const reqBody = await req.json();
         await connectToDatabase();
-        const result = await prisma.post.findMany({
-            orderBy: {
-                createdAt: 'desc'
-            },include:{comments:true}
+        const result = await prisma.massage.create({
+          data: reqBody 
         });
         return NextResponse.json({
             status: "success",
-            message: "Blog post fetched successfully",
+            message: "Massage is sent successfully",
             data: result
         });
 
     }catch(error){
         console.log(error);
         return NextResponse.json({
-            message: "Something went wrong with the blog post"
+            message: "Failed to send the massage",
+            error: error
         });
     }
     finally{
         await prisma.$disconnect();
 
     }
+
 }
