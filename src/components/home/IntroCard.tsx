f44ae5b63957bfc25989e24bca801b3881443372
Najmul Islam Naeem <csenajmulislamnaeem@gmail.com>
2024-07-05 10:54:15 +0600

    mainGrid comonents is modifyed for text size

--- src/components/home/IntroCard.tsx
+++ src/components/home/IntroCard.tsx
@@ -4,11 +4,10 @@ const IntroCard = () => {
   return (
     <section className="w-full mt-[30px] container">
       <div className="p-2 text-justify">
-        <p className="mt-8"> Hi ! There I &#39; m </p>
-        <h1 className="uppercase no-underline mt-[20px] text-[50px] ">
-          {" "}
-          Professional{" "}
-          <span className="text-primary"> Full-Stack <br/> Web Developer </span>{" "}
+        <p className="mt-8 text-center"> <span className={"text-primary"}> Hi ! </span> There I&#39;m </p>
+        <h1 className="uppercase no-underline mt-[20px] text-xl md:text-[50px] ">
+          Professional
+          <span className="text-primary"> Full-Stack </span>  <span className={" md:mt-5 block"}> Web Developer </span>
         </h1>
         <p className="text-muted my-10">
           Full Stack Web Developer and Web Application specializing in front-end
